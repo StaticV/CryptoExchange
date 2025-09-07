622b088e282e1c3ff4987c7d81942622542e7b75
stati <stati@DESKTOP-E2V8H3Q>
2025-09-07 16:35:46 -0400

    first

---
+++ CryptoExchange/src/com/github/staticv/cryptoexchange/ExchangeException.java
@@ -0,0 +1,9 @@
+package com.github.staticv.cryptoexchange;
+
+@SuppressWarnings("serial")
+public class ExchangeException extends Exception {
+
+	public ExchangeException(String message) {
+		super(message);
+	}
+}
